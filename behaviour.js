f11f0f840003557bbb36395a71826d45a572cf09
thormiwa <thormiwa04@gmail.com>
2021-09-29 23:12:54 +0100

    food plug

---
+++ behaviour.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+    thumbnailElement.addEventListener("click", function() {
+        alert("I saw you click!");
+      });
+      <div id="disqus_thread"></div>
+<script>
+ disqus_config = function () {
+this.page.url = http://3.237.11.195/main.html;  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = //3.237.11.195/main.html; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://foodplug-2.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
+<script>
+var disqus_config = function () {
+this.page.url =" http://3.237.11.195/main.html" ;
+this.page.identifier ="//3.237.11.195/main.html" ;
+};
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://foodplug-2.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+ </aside>
+</main>
+<footer>
+  <p>&copy; Made by Okafor Nneka Tomiwa for <a href="https://alx-apply.hbtn.io"
+    target="_blank">ALX</a></p>
+</footer>
+<script id="dsq-count-scr" src="//foodplug-2.disqus.com/count.js" async></script>
+</body>
+
+                            
+});
